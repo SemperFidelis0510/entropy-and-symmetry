d59c07b6cb248186691241a81c2c73961676a1c9
Jingqin Tang <104819614+GWWGWD@users.noreply.github.com>
2023-08-10 11:45:02 +0300

    Update cal_ent_temp.py
    new version!!

--- scripts/cal_ent_temp.py
+++ scripts/cal_ent_temp.py
@@ -7,18 +7,16 @@ def calc_ent(img_arr, method):
     match method:
         case 'hist_grey':
             bins_ = 256
-            hists, bins = np.histogram(img_arr.ravel(), bins=bins_, range=(0, bins_))
-            hists_done = hists / hists.sum()
-            img_entropy = entropy(hists_done)
+            hist, bins = np.histogram(img_arr.ravel(), bins=bins_, range=(0, bins_ - 1))
+            hist_done = hist / hist.sum()
+            img_entropy = entropy(hist_done)
         case 'hist_RGB':
-            bins_ = 256
-            r = img_arr[:, :, 0]
-            g = img_arr[:, :, 1]
-            b = img_arr[:, :, 2]
-            total = np.concatenate((r, g, b))
-            hists_RGB, bins = np.histogram(total, bins=bins_, range=(0, bins_))
-            hists_RGB_done = hists_RGB / hists_RGB.sum()
-            img_entropy = entropy(hists_RGB_done)
+            bins_ = 256 ** 3
+            flattened_img_arr = (img_arr[:, :, 0] << 16) + (img_arr[:, :, 1] << 8) + img_arr[:, :, 2]
+            hist, _ = np.hisogram(flattened_img_arr, bins=bins_, range=(0, bins_ - 1))
+            hist_done = hist / np.sum(hist)
+            img_entropy = -np.sum(hist_done * np.log(hist_done + np.finfo(float).eps))
+
     return img_entropy
 
 
