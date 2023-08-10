9ab8f3026f0c482145e7bdec3f1b67843b6cf992
Bar Tavor <mtavor556@gmail.com>
2023-08-10 11:57:27 +0300

    x

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,17 +1,27 @@
-import numpy as np
-from scipy.stats import entropy
-from PIL import Image
 import os
+
+import numpy as np
+from PIL import Image
+from scipy.stats import entropy
 
 
 def calc_ent(img_arr, method):
     img_entropy = 0
     match method:
-        case 'hist':
+        case 'hist_greyscale':
             bins_ = 256
             hists, bins = np.histogram(img_arr.ravel(), bins=bins_, range=(0, bins_))
             hists_done = hists / hists.sum()
             img_entropy = entropy(hists_done)
+        case 'hist':
+            bins_ = 256 ** 3
+            r = img_arr[:, :, 0]
+            g = img_arr[:, :, 1]
+            b = img_arr[:, :, 2]
+            total = np.concatenate((r, g, b))
+            hists_RGB, bins = np.histogram(total, bins=bins_, range=(0, bins_))
+            hists_RGB_done = hists_RGB / hists_RGB.sum()
+            img_entropy = entropy(hists_RGB_done)
     return img_entropy
 
 
