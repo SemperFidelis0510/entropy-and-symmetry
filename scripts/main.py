6447f2fec0cfa4b8c4d0ac7a5bc750221d9d10e2
semperfidelis0510 <mtavor556@gmail.com>
2023-08-13 16:01:08 +0300

    improve interface

--- scripts/main.py
+++ scripts/main.py
@@ -7,19 +7,20 @@ def sort_folder(path):
     # colors = 'greyscale'
     colors = 'rgb'
     method = 'dft'
-    dst_folder = f'../processed/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+    dst_folder = f'../processed/m={method}_t={datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
 
     img_arrays = preprocess(path, colors=colors)
     img_arrays = label_ent(img_arrays, method)
 
     sorted_list = sorted(img_arrays, key=lambda x: x[1])
-    print('The images are sorted by entropy.')
+    print('\nThe images are sorted by entropy.')
 
     save_img(dst_folder, sorted_list)
 
 
 def main():
-    folder_path = r'../datasets/pattern_images'
+    # folder_path = r'../datasets/pattern_images'
+    folder_path = r'../datasets/web_symmetries/rotation'
     # folder_path = r'../datasets/Fractals with controlled parameter/Line-Replacement Fractals/snowflake'
     sort_folder(folder_path)
 
