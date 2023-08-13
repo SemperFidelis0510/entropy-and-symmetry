dc9a46178690263f5b2dc3a66598de318187c6a7
汤景钦 <tang09400@gtiit.edu.cn>
2023-08-13 10:28:19 +0300

    apply noise function. put all in main.py. havenot been tested

---
+++ scripts/apply_noise.py
@@ -0,0 +1,11 @@
+import random
+def apply_noise(img_arr):
+    new_imgs = []
+    dst_folder = f'../processed/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+    num_of_images = random.randint(1,100)
+    for i in range(1,num_of_images + 1):
+        level = random.random()
+        new_img_arr = uniform_noise(img_arr, level)
+        new_imgs.append(new_img_arr)
+    save_img(dst_folder, new_imgs)
+    return
