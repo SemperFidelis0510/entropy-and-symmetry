fixes and normalization

scripts/entropy.py
@@ -1,6 +1,5 @@
 import math
 
-import matplotlib.pyplot as plt
 from scipy.signal import convolve2d
 from skimage.color import rgb2gray
 from skimage.feature import graycomatrix
@@ -9,6 +8,7 @@ from skimage.measure import shannon_entropy
 from skimage.segmentation import slic
 
 from transforms import *
+
 
 def label_ent(images, method, sort=True):
     """
@@ -137,9 +137,13 @@ def histogram(img_arr, color='rgb'):
 
 
 def calculate_GLCM_entropy(image):
-    distances = [1]  # Distance between pixels for co-occurrence
-    angles = [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4]  # Angles for co-occurrence (in radians)
-    levels = 256  # Number of intensity levels in the image
+    # Check if the image is 3D (e.g., RGB) and convert to grayscale if necessary
+    if len(image.shape) == 3:
+        image = rgb2gray(image)
+
+    distances = [1]
+    angles = [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4]
+    levels = 256
 
     gray_image = (image * 255).astype(np.uint8)
     glcm = graycomatrix(gray_image, distances=distances, angles=angles, levels=levels, symmetric=False, normed=True)
@@ -147,7 +151,7 @@ def calculate_GLCM_entropy(image):
     total_entropy = 0
     for angle_idx in range(len(angles)):
         matrix = glcm[:, :, 0, angle_idx]
-        matrix = matrix / np.sum(matrix)  # Normalize matrix to probabilities
+        matrix = matrix / np.sum(matrix)
         entropy = -np.sum(matrix * np.log2(matrix + np.finfo(float).eps))
         total_entropy += entropy
 
@@ -242,6 +246,10 @@ def calculate_texture_entropy(img_arr):
         gray_image = rgb2gray(img_arr)
     else:
         gray_image = img_arr
+
+    # Convert to 8-bit integer type
+    gray_image = (gray_image * 255).astype(np.uint8)
+
     # Apply Local Binary Pattern (LBP) to extract texture features
     radius = 1
     n_points = 8 * radius
@@ -306,6 +314,7 @@ def adaptive_entropy_estimation(img_arr, num_segments=100):
 def laplace_ent(image):
     return entropy(laplacian(image))
 
+
 def calculate_dwt_entropy(image, wavelet='db1', level=1):
-    w_transform_arr = dwt(image,wavelet,level)
+    w_transform_arr = dwt(image, wavelet, level)
     return entropy(w_transform_arr)

scripts/main.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 from entropy import *
@@ -31,7 +32,38 @@ def sort_by_noise(path):
 
     print('\nThe images are sorted by entropy.')
 
-    save_img(dst_folder, img_arrays)
+    save_img(dst_folder, sorted_list)
+
+
+def norm_ent():
+    ENTROPY_METHODS = [
+        'hist',
+        'hist_greyscale',
+        'naive',
+        'dft',
+        'dwt',
+        'laplace',
+        'joint_red_green',
+        'joint_all',
+        'lbp',
+        'lbp_gabor',
+        'adapt',
+        'GLCM',
+        'RGBCM_each_channel',
+        'RGBCM_to_gray'
+    ]
+    ENTROPY_METHODS = [
+        'lbp'
+    ]
+
+    path = '../datasets/fixed_noise.bmp'
+    img_arr = np.array(Image.open(path))
+    N = {}
+    for ent in ENTROPY_METHODS:
+        N[ent] = calc_ent(img_arr, ent)
+
+    with open('ent_norm', 'w') as file:
+        json.dump(N, file)
 
 
 def main():
@@ -41,10 +73,12 @@ def main():
     folder_path = '../datasets/noising'
 
     folder_path = normalize_path(folder_path)
-    print(f'Dataset path: {os.path.abspath(folder_path)}')
+    # print(f'Dataset path: {os.path.abspath(folder_path)}')
 
     # sort_folder(folder_path)
-    sort_by_noise(folder_path)
+    # sort_by_noise(folder_path)
+
+    norm_ent()
 
 
 if __name__ == '__main__':