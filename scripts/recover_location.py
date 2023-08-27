244694ad78a42914770f282fa2d3015f93fac1f8
Yanglin Zhang <arlinzhang@163.com>
2023-08-28 02:15:38 +0800

    Update recover

--- scripts/recover_location.py
+++ scripts/recover_location.py
@@ -1,31 +1,44 @@
 import os
 import hashlib
 from shutil import copyfile
+from PIL import Image
+import numpy as np
 
+def calculate_sha256(image_path):
+    """Calculate SHA-256 hash of the first 100 RGB pixels of an image."""
+    img = Image.open(image_path).convert('RGB')
+    img_array = np.array(img)
 
-def calculate_sha256(file_path):
-    """Calculate SHA-256 hash of a file."""
+    # Flatten the first 100 RGB pixels into a 1D array
+    flat_pixels = img_array[:10, :10, :3].flatten()
+
+    # Convert to bytes
+    pixel_bytes = flat_pixels.tobytes()
+
+    # Calculate SHA-256 hash
     sha256_hash = hashlib.sha256()
-    with open(file_path, "rb") as f:
-        # Read file in chunks of 4K
-        for byte_block in iter(lambda: f.read(4096), b""):
-            sha256_hash.update(byte_block)
+    sha256_hash.update(pixel_bytes)
     return sha256_hash.hexdigest()
 
 
 # Path to the datasets folder and source folder
 datasets_path = "../datasets/classified_pictures"
-source_path = "path/to/source/folder"
+image_directory = "../datasets/satellite"
 
 # Create a dictionary to store SHA-256 hashes of source files
 source_hashes = {}
-
+image_paths = []
+if os.path.isdir(image_directory):
+    for root, _, filenames in os.walk(image_directory):
+        for filename in filenames:
+            if filename.lower().endswith(('png')):
+                img_path = os.path.join(root, filename)
+                image_paths.append(img_path)
 # Calculate SHA-256 hashes for all files in the source folder
-for filename in os.listdir(source_path):
-    file_path = os.path.join(source_path, filename)
-    if os.path.isfile(file_path):
-        file_hash = calculate_sha256(file_path)
-        source_hashes[file_hash] = filename
+for path in image_paths:
+    file_hash = calculate_sha256(path)
+    print(f"Calculated hash for {path}: {file_hash}")  # Debugging line
+    source_hashes[file_hash] = path
 
 # Loop through each subfolder in the datasets folder
 for subfolder in os.listdir(datasets_path):
@@ -45,14 +58,15 @@ for subfolder in os.listdir(datasets_path):
 
         # Calculate the SHA-256 hash of the file
         file_hash = calculate_sha256(file_path)
+        print(f"Looking up hash for {file_path}: {file_hash}")  # Debugging line
 
         # Check if this hash exists in the source folder
         if file_hash in source_hashes:
-            original_name = source_hashes[file_hash]
-
+            original_name = source_hashes[file_hash]  # Debugging line
+            new_file_name = os.path.basename(original_name)
+            new_file_dir = os.path.dirname(filename)
             # Construct the new path for the file
-            new_file_path = os.path.join(subfolder_path, original_name)
-
+            new_file_path = os.path.join(new_file_dir, new_file_name)
+            print(f"Match found for {file_path}. Renaming to {new_file_path}.")
             # Rename the file
             os.rename(file_path, new_file_path)
-
