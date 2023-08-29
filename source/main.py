76f3e7cbd1441efdb05d65c6fe3e18157b971864
Yanglin Zhang <99948380+yangl1n@users.noreply.github.com>
2023-08-29 11:45:33 +0300

    Update main.py to auto save

--- source/main.py
+++ source/main.py
@@ -39,14 +39,16 @@ def main(dst_folder=None, src_folder=None, process_methods_with_params=None):
     transformer = Processor(process_methods_with_params)
     entropyCalculator = EntropyCalculator(color_weight=None)
     postprocessor = Postprocessor(postprocessor_methods)
-    dataSaver = DataSaver(destination=dst_folder, methods=list(process_methods_with_params.keys()))
+    dataSaver = DataSaver(destination=dst_folder, methods=list(process_methods_with_params.keys())
+                         , auto_save=True)
 
     # Initialize PipelineManager
     pipeline = PipelineManager(imageLoader, preprocessor, transformer,
                                entropyCalculator, postprocessor, dataSaver)
 
     # Run
-    pipeline.runPipeline()
+    pipeline.runAutoSavePipeline()
+    # pipeline.runPipeline()
     # pipeline.runParallelPipeline(batch_size=150)
 
 
