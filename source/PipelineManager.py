938c92b752c59a1cd15d372795acbfc8f88256df
Yanglin Zhang <99948380+yangl1n@users.noreply.github.com>
2023-08-29 09:19:01 +0300

    Update PipelineManager.py add autosave pipeline

--- source/PipelineManager.py
+++ source/PipelineManager.py
@@ -46,6 +46,27 @@ class PipelineManager:
         self.dataSaver.save_ent_result(sorted_images)
 
         open_folder(self.dataSaver.destination)
+
+    
+    def runAutoSavePipeline(self, max_queue_size=30): #Saved result when the queue is full
+        image_objects = self.imageloader.load_images()
+
+        # Step 1: Apply preprocessing, transformations, and entropy calculations on all images
+        n = len(image_objects)
+        start_time = time.time()
+        queue_images = []
+        for index, image_object in enumerate(image_objects):
+            self.process_single_image(image_object)
+            queue_images.append(image_object)
+            if len(queue) == max_queue_size:
+                for index, image_object in enumerate(queue_images):
+                    self.dataSaver.save(index, image_object)
+                self.dataSaver.auto_save_ent_result(queue_images)
+                queue_images = []
+            print_progress_bar('Entropy calculation', index+1, n, start_time=start_time)
+        print(f'\nEntropy calculation done.')
+
+        
     def process_and_save_single_image(self, image, index):
         # Step 1: Process the image
         self.process_single_image(image)
@@ -57,6 +78,8 @@ class PipelineManager:
         self.dataSaver.save_single_ent_result(index, image)
         del image
         gc.collect()
+
+    
     def runParallelPipeline(self, batch_size=300):
         images = self.imageloader.load_images()
         num_images = len(images)
