e5b69c6fbfc1a5fb863e4d53f3ea39fe9124f165
Zixuan Guan <115603775+NewTempestStu@users.noreply.github.com>
2023-09-04 15:22:42 +0300

    Update GUI

--- source/gui_stable_version.py
+++ source/gui_stable_version.py
@@ -150,7 +150,7 @@ class ImageViewer:
         self.image_window = Toplevel()
         self.image_window.title("Image Viewer")
         self.image_window.protocol('WM_DELETE_WINDOW', lambda: self.thread_it(self.clos_window))
-        self.image_window.geometry(self.center_window_coordinates(600, 632))  # The desired initial size
+        self.image_window.geometry(self.center_window_coordinates(1023, 632))  # The desired initial size
         self.create_menu()
         self.create_image_frame()
         self.create_thumbnail_frame()
@@ -334,35 +334,42 @@ class ImageViewer:
 
 
     def create_thumbnail_frame(self):
-        frame_thumbnails_container = Frame(self.image_window, width=60)  # Adjust width
-        frame_thumbnails_container.grid(row=0, column=0, rowspan=3, sticky="ns")
-        frame_thumbnails_container.grid_propagate(False)  # Forbid internal components to change size
-        self.canvas_thumbnails = Canvas(frame_thumbnails_container, width=60, height=600)
-        self.canvas_thumbnails.pack(side=LEFT, fill=BOTH, expand=True)
-        scrollbar = Scrollbar(frame_thumbnails_container, orient="vertical")
-        scrollbar.config(command=self.on_scroll)
-
         if self.os_name == 'Darwin':
             gap = 8
         if self.os_name == 'Linux':
             gap = 6
         if self.os_name == 'Windows':
             gap = 6
-        
+        thumbnail_size = 60  # assume thumbnail size is 60
+        columns = 5
+        frame_width = columns * thumbnail_size + (columns) * gap
+    
+        frame_thumbnails_container = Frame(self.image_window, width=frame_width)
+        frame_thumbnails_container.grid(row=0, column=0, rowspan=3, sticky="ns")
+        frame_thumbnails_container.grid_propagate(False)
+
+        self.canvas_thumbnails = Canvas(frame_thumbnails_container, width=frame_width, height=600)
+        self.canvas_thumbnails.pack(side=LEFT, fill=BOTH, expand=True)
+        scrollbar = Scrollbar(frame_thumbnails_container, orient="vertical")
+        scrollbar.config(command=self.on_scroll)
+
         scrollbar.pack(side=RIGHT, fill=Y)
         self.canvas_thumbnails.config(yscrollcommand=scrollbar.set)
-        self.frame_thumbnails = Frame(self.canvas_thumbnails, width=60,
-                                      height=len(self.List_thumbnail_images) * (60+gap))  # Increase height
+    
+        rows_required = (len(self.np_array) + columns - 1) // columns
+        self.frame_thumbnails = Frame(self.canvas_thumbnails, width=frame_width, height=rows_required * (thumbnail_size+gap))
         self.canvas_thumbnails.create_window((0, 0), window=self.frame_thumbnails, anchor='nw')
 
         for idx in range(len(self.np_array)):
+            row_idx = idx // columns
+            col_idx = idx % columns
             thumbnail_button = Button(self.frame_thumbnails, image=self.thumbnail_placeholder, relief=FLAT, command=lambda i=idx: self.on_select(i))
-            thumbnail_button.grid(row=idx, column=0, sticky='nsew', padx=0, pady=0, ipadx=0, ipady=0)
-            self.frame_thumbnails.rowconfigure(idx, weight=1)
-
+            thumbnail_button.grid(row=row_idx, column=col_idx, sticky='nsew', padx=0, pady=0, ipadx=0, ipady=0)
+            self.frame_thumbnails.rowconfigure(row_idx, weight=1)
+            self.frame_thumbnails.columnconfigure(col_idx, weight=1)
 
         self.canvas_thumbnails.config(
-            scrollregion=(0, 0, 60, len(self.List_thumbnail_images) * (60+gap)))  # Resize the scrolling area
+            scrollregion=(0, 0, frame_width, rows_required * (thumbnail_size+gap)))
         self.canvas_thumbnails.bind('<Configure>', self.load_visible_thumbnails)
         self.canvas_thumbnails.bind('<Enter>', self.load_visible_thumbnails)
 
@@ -494,10 +501,15 @@ class ImageViewer:
         # Get the scroll position
         top = self.canvas_thumbnails.canvasy(0)
         height = self.canvas_thumbnails.winfo_height()
+        columns = 5  # Number of columns
+        thumbnail_height = 60
 
         # Calculate the index range of thumbnails that should be loaded
-        start_idx = max(int(top // 60) - 2, 0)
-        end_idx = min(int((top + height) // 60) + 2, len(self.np_array))
+        start_row = max(int(top // thumbnail_height) - 2, 0)
+        end_row = min(int((top + height) // thumbnail_height) + 2, -(-len(self.np_array) // columns))  # Ceiling division
+
+        start_idx = start_row * columns
+        end_idx = min(end_row * columns, len(self.np_array))
 
         # Load and update thumbnails for visible index ranges
         for idx in range(start_idx, end_idx):
